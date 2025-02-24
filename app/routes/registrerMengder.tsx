ee05eb5684ad904e79600ed093896348f6bf3903
CarlFMørch-Kontny <carlf.kontny@hotmail.com>
2025-02-24 11:02:15 +0100

    ny rute registrerMengder

---
+++ app/routes/registrerMengder.tsx
@@ -0,0 +1,10 @@
+export default function RegistrerMengder() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-500">
+      <h1>Registrer Mengder</h1>
+      <form action="">
+        <input type="email" placeholder="hansen@gmail.com" />
+      </form>
+    </div>
+  );
+}
